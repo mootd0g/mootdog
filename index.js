bca901a03621402db7354b4876e8b3e3303e9c06
Andrey Shagov <writeletterto@me.com>
2016-12-10 22:09:45 +0300

    add server

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-var пидор = "pidr";
-console.log(пидор);
+var express = require('express');
+var app = express();
+
+
+app.use('/', express.static('public'));
+
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App started on port ${process.env.PORT || 3000}`);
+});
